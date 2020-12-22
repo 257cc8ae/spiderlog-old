e8b0ce0de946eac5415acb090e06a8a80e48d30c
Lusaca <kny70dr@gmail.com>
2020-12-22 21:50:31 +0900

    add:Enabling to get the list of Scss files

--- script.py
+++ script.py
@@ -1,14 +1,23 @@
 import sys
+import glob
 import modules.message
+import re
 def build():
-    print("build function")
+    files = glob.glob("./assets/stylesheets/**", recursive=True)
+    re_scss_file_path = re.compile(r".*.scss")
+    for file_name in files:
+        if re_scss_file_path.fullmatch(file_name):
+            print(file_name)
 
 def main():
-    PROCESS_NAME: str = sys.argv[1]
-    if PROCESS_NAME == "build":
-        build()
-    else:
-        modules.message.error(f"Could not find PROCESS NAME \"{PROCESS_NAME}\"")
+    try:
+        PROCESS_NAME: str = sys.argv[1]
+        if PROCESS_NAME == "build":
+            build()
+        else:
+            modules.message.error(f"Could not find PROCESS NAME \"{PROCESS_NAME}\"")
+    except IndexError:
+        modules.message.error("No argument is specified.")
 
 if __name__ == "__main__":
     main()
