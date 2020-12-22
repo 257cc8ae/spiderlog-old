1065ab14414882717fd4a7d8f64919dc1fa69f42
Lusaca <kny70dr@gmail.com>
2020-12-22 22:30:32 +0900

    add:scss file compile

--- script.py
+++ script.py
@@ -1,14 +1,31 @@
 import sys
 import glob
-import modules.message
 import re
+import sass
+import os
+import modules.directory
+import modules.message
+
 def build():
+    # os.makedirs(os.path.join("dist","stylesheets"), exist_ok=True)
     files = glob.glob("./assets/stylesheets/**", recursive=True)
-    re_scss_file_path = re.compile(r".*.scss")
+    re_scss_file_path = re.compile(r".\/assets\/stylesheets\/(.*).scss")
     for file_name in files:
         if re_scss_file_path.fullmatch(file_name):
-            print(file_name)
+            print(os.path.join("dist","stylesheets",f"{re_scss_file_path.match(file_name).group(1)}.css"))
+            with open(file_name, "r") as f:
+                try:
+                    css_code = sass.compile(string = f.read(), output_style='compressed')
+                    print(css_code)
+                    modules.directory.new(os.path.join("dist","stylesheets",f"{re_scss_file_path.match(file_name).group(1)}.css"))
+                    # with open(os.path.join("dist","stylesheets",f"{re_scss_file_path.match(file_name).group(1)}.css"),"w") as css_file:
+                    #     f.write(css_code)
+                except sass.CompileError:
+                    pass
 
+                
+                
+            
 def main():
     try:
         PROCESS_NAME: str = sys.argv[1]
