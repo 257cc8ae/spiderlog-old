79cfdd12a580392465d726303ca9f78aa8c6f41c
Lusaca <kny70dr@gmail.com>
2020-12-23 22:41:41 +0900

    add: select stylesheets type

--- modules/new.py
+++ modules/new.py
@@ -9,5 +9,14 @@ def main():
     if os.path.exists(PROJECT_NAME):
         modules.message.warn(f"Directory name is \"{PROJECT_NAME}\" with the same name directory exists.")
         overwrite_and_continue = input("Do you want to overwrite and continue? (Y or anything else keys):")
-    if overwrite_and_continue != "Y" or overwrite_and_continue != "y":
-        modules.message.error("Naming error (Intentional termination by you)")     
+        # issues Y or N が正しく判定されない
+        if overwrite_and_continue != "Y" or overwrite_and_continue != "y":
+            modules.message.error("Naming error (Intentional termination by you)")
+            sys.exit()
+    stylesheet_type = input("Which stylesheet types do you use, CSS or SCSS or SASS?:")
+    while not stylesheet_type.upper() in ["CSS","SCSS","SASS"]:
+        stylesheet_type = input("Which stylesheet types do you use, CSS or SCSS or SASS?:")
+        print("google")
+        
+    
+    modules.message.success("Finished making your new project.")
