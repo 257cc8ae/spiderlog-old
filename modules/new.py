e63dbecef8313c4f7a95ed48b187c52c60490d00
Lusaca <kny70dr@gmail.com>
2020-12-24 15:28:29 +0900

    fix:overwrite and continue Judgment

--- modules/new.py
+++ modules/new.py
@@ -9,8 +9,7 @@ def main():
     if os.path.exists(PROJECT_NAME):
         modules.message.warn(f"Directory name is \"{PROJECT_NAME}\" with the same name directory exists.")
         overwrite_and_continue = input("Do you want to overwrite and continue? (Y or anything else keys):")
-        # issues Y or N が正しく判定されない
-        if overwrite_and_continue != "Y" or overwrite_and_continue != "y":
+        if overwrite_and_continue.upper() != "Y":
             modules.message.error("Naming error (Intentional termination by you)")
             sys.exit()
     stylesheet_type = input("Which stylesheet types do you use, CSS or SCSS or SASS?:")
