36313469ccbc6b6761bdf914c7c74b7618d1a623
Lusaca <kny70dr@gmail.com>
2020-12-27 22:52:06 +0900

    fix: Enhanced readability of code for directory creation functions

--- modules/new.py
+++ modules/new.py
@@ -6,6 +6,7 @@ import shutil
 import subprocess
 import json
 
+
 def main():
     try:
         PROJECT_NAME = sys.argv[2]
@@ -22,7 +23,7 @@ def main():
             modules.message.error(
                 "Naming error (Intentional termination by you)")
             sys.exit()
-    os.makedirs(PROJECT_NAME,exist_ok=True)
+    os.makedirs(PROJECT_NAME, exist_ok=True)
     os.chdir(PROJECT_NAME)
     stylesheet_type = input(
         "Which stylesheet types do you use, CSS or SCSS or SASS?:")
@@ -35,20 +36,29 @@ def main():
     if modules.verification_values.yes_or_no(using_git):
         try:
             subprocess.check_call("git --version", shell=True)
-            subprocess.call("git init",shell=True)
-            git_remote_url = input("Input your git remote repository url (If you do not have it,please input \"none\"):")
+            subprocess.call("git init", shell=True)
+            git_remote_url = input(
+                "Input your git remote repository url (If you do not have it,please input \"none\"):")
             if git_remote_url.lower() != "none":
-                subprocess.call(f"git remote add origin {git_remote_url}", shell=True)
+                subprocess.call(
+                    f"git remote add origin {git_remote_url}", shell=True)
         except subprocess.CalledProcessError:
             modules.message.error("Git is not installed.")
-            modules.message.message("Please check how to install git. https://git-scm.com/download")
-    os.makedirs("assets/stylesheets", exist_ok=True)
-    os.makedirs("assets/images", exist_ok=True)
-    os.makedirs("javascripts", exist_ok=True)
-    os.makedirs("static", exist_ok=True)
-    os.makedirs("pages", exist_ok=True)
-    os.makedirs("layouts", exist_ok=True)
-    os.makedirs("components", exist_ok=True)
+            modules.message.message(
+                "Please check how to install git. https://git-scm.com/download")
+    new_directories = [
+        "assets/stylesheets",
+        "assets/images",
+        "javascripts",
+        "static",
+        "pages",
+        "layouts",
+        "components",
+        "config",
+    ]
+    for new_directory in new_directories:
+        os.makedirs(new_directory, exist_ok=True)
+
     configuration = {
         "version": "0.0.1",
         "name": PROJECT_NAME,
@@ -61,6 +71,6 @@ def main():
             "path": "./static/favicon.png"
         }
     }
-    with open("sl.json", "w",encoding="utf-8") as f:
+    with open("sl.json", "w", encoding="utf-8") as f:
         json.dump(configuration, f)
     modules.message.success("Finished making your new project.")
